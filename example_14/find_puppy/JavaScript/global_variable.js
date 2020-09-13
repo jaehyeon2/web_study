54b9253aa6efb539a4ad3ba4ba83469bff8fc6f7
강재현 <wol5971@gmail.com>
2020-09-13 08:09:26 +0000

    initial commit

--- example_14/find_puppy/JavaScript/global_variable.js
+++ example_14/find_puppy/JavaScript/global_variable.js
@@ -1,6 +1,11 @@
-var gameMenu = document.getElementById("gameMenu");
-var restMenu = document.getElementById("restMenu");
-var failMenu = document.getElementById("failMenu");
-var timeMenu = document.getElementById("timeMenu");
-var statusMenu = document.getElementById("statusMenu");
-var gameWindow = document.getElementById("gameWindow");
+var gameMenu = document.getElementById("gameMenu");//게임 메뉴 버튼
+var restMenu = document.getElementById("restMenu");//남은 갯수
+var failMenu = document.getElementById("failMenu");//틀린 횟수
+var timeMenu = document.getElementById("timeMenu");//남은 시간
+var statusMenu = document.getElementById("statusMenu");//게인 진행 상태
+var gameWindow = document.getElementById("gameWindow");//게임 창
+var time=5;//카운트 다운 초기 시간
+var gaming="false"//게임 진행 여부
+var gameover = 0;//게임 오버 여부
+var eggs = new Array(24);//정답 eggs 저장
+var chosenEggs = 0;//뽑힌 eggs 개수 저장
