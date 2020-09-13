5e600848b4749014f631e7d2d777a986c32310ed
강재현 <wol5971@gmail.com>
2020-09-13 07:15:23 +0000

    initial commit

---
+++ example_14/find_puppy/JavaScript/gameControl.js
@@ -0,0 +1,8 @@
+function gameStart(){
+	init();
+	gameMenu.style.visibility='hidden';
+}
+function init(){
+	gameMenu.innerHTML = "게임 시작";
+	gameMenu.style.visibility='visible';
+}
---
+++ example_14/find_puppy/JavaScript/global_variable.js
@@ -0,0 +1,6 @@
+var gameMenu = document.getElementById("gameMenu");
+var restMenu = document.getElementById("restMenu");
+var failMenu = document.getElementById("failMenu");
+var timeMenu = document.getElementById("timeMenu");
+var statusMenu = document.getElementById("statusMenu");
+var gameWindow = document.getElementById("gameWindow");
