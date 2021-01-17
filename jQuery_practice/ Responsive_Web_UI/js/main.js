180ff9878d3988e24e1753405a6580b69ab303ac
강재현 <wol5971@gmail.com>
2021-01-17 09:35:59 +0000

    typo revise

--- jQuery_practice/ Responsive_Web_UI/js/main.js
+++ jQuery_practice/ Responsive_Web_UI/js/main.js
@@ -10,12 +10,12 @@ $(function(){
 	$("#notice-tab-wrap h4 a").on("click", tabmenu);
 	function tabmenu(e){
 		e.preventDefault();
-		var $ts=$(this);
-		var $next=$ts.parent().next();
-		if($next.is(":hiden")){
+		var $ts = $(this);
+		var $next = $ts.parent().next();
+		if($next.is(":hidden")){
 			$("#notice-tab-wrap h4 a").removeClass("on");
 			$ts.addClass("on");
-			$("notice-tab-wrap>div:visible").hide();
+			$("#notice-tab-wrap > div:visible").hide();
 			$next.show();
 		}
 	}
