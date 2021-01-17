cfb8d09d35adf1c749e511ea22bab3bd2f21bed0
강재현 <wol5971@gmail.com>
2021-01-17 09:18:30 +0000

    tab_menu code add

--- jQuery_practice/ Responsive_Web_UI/js/main.js
+++ jQuery_practice/ Responsive_Web_UI/js/main.js
@@ -6,5 +6,18 @@ $(function(){
 		autoControls:true,
 		autocontrolsCombine:true
 	});
-	//...
+	
+	$("#notice-tab-wrap h4 a").on("click", tabmenu);
+	function tabmenu(e){
+		e.preventDefault();
+		var $ts=$(this);
+		var $next=$ts.parent().next();
+		if($next.is(":hiden")){
+			$("#notice-tab-wrap h4 a").removeClass("on");
+			$ts.addClass("on");
+			$("notice-tab-wrap>div:visible").hide();
+			$next.show();
+		}
+	}
+	
 })
