60f57301eace32ae8d97580f4d40afa83dbe3b5c
강재현 <wol5971@gmail.com>
2021-01-24 09:33:40 +0000

    menu change js code

---
+++ web_publishing/js/ui.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("nav li").click(function(){
+		var id=$(this).attr("data-rol");
+		$("nav li").removeClass("on");
+		$(this).addClass("on");
+		//alert($(this).attr("data-rol")); 어떤 data-rol값이 나오는지 확인
+		$(".content").removeClass("prev this next");
+		//클릭 시 가지고 있던 클래스를 삭제
+		$("#"+id).addClass("this").prevAll().addClass("prev");
+		//클릭 한 메뉴와 매칭되는 id에 this 클래스를 부여, 앞의 모든 메뉴에 prev 클래스 부여
+		$("#"+id).nextAll().addClass("next");
+		//뒤의 모든 메뉴에 next 클래스를 부여
+	})
+})
